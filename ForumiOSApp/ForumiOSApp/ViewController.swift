55c8f221f39e08fb34888d7422a21c71706fb2f2
Artemy Markovsky <artemexe2013@gmail.com>
2019-09-16 16:48:20 +0300

    ios app init

---
+++ ForumiOSApp/ForumiOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForumiOSApp
+//
+//  Created by Artemy Markovsky on 9/15/19.
+//  Copyright © 2019 Artemy Markovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
